9d4758b8b49eabefe59218b747f7fd5fc606a6f6
Jessica Antunes <jessantunes@protonmail.com>
2021-02-23 15:53:52 -0500

    commit tree 37d8f64b3e9ca9595c53da1c8e907dc86f4521d8

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
 
+    if array.length == 2
+        array.join(" and ")
+    elsif array.length < 2
+        array.join
+    elsif array.length > 2
+        last_element = array.pop()
+        array.join(", ")<<(", and " + last_element)
+    end
+
 end
